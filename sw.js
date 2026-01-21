46c2ffe4d278d6f4629a17b308074ae48c6fc10a
websiteabdus-ship-it <websiteabdus@gmail.com>
2026-01-21 23:23:44 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "abdussalam-pwa-v1";
+const URLS = [
+  "/website/",
+  "/website/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+
+  );self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  clients.claim();
+});
+});
